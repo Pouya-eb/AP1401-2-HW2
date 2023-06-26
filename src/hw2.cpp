7a28d24a19d6726302feac9aa6bd54e4334ae312
Pouya-eb <Pouyaebrahimi1379@gmail.com>
2023-06-26 20:02:10 +0330

    Completing Login constructor, destructor and readFile function

--- src/hw2.cpp
+++ src/hw2.cpp
@@ -13,3 +13,35 @@ User::User(std::string name, std::string pass, std::string mail)
 User::~User()
 {
 }
+
+//********** Login **********//
+
+Login::Login() { }
+Login::~Login() { }
+
+void Login::readFile(std::string path)
+{
+    std::string name, pass, mail, login;
+    int cnt { -1 };
+    std::ifstream file { "users.txt" };
+    if (file.fail())
+        std::cout << "There is a problem reading the file" << std::endl;
+
+    std::string line;
+    if (file.is_open()) {
+        for (;;) {
+            cnt++;
+            if (!getline(file, line))
+                break;
+            if (cnt == 0)
+                continue;
+            std::stringstream tmp(line);
+            tmp >> name >> pass >> mail >> login;
+            User* usr = new User { name, pass, mail };
+            users.push_back(usr);
+            if (login == "yes") {
+                LoginedUsers.push_back(usr);
+            }
+        }
+    }
+}
