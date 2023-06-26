65163208011187f7248b6a93228fb1b1a7aa9915
Pouya-eb <Pouyaebrahimi1379@gmail.com>
2023-06-26 20:00:32 +0330

    Completing User constructor and destructor

--- include/hw2.h
+++ include/hw2.h
@@ -1,7 +1,10 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
 #include <string>
 #include <vector>
 
-struct User{
+struct User {
     User(std::string name, std::string pass, std::string mail);
     ~User();
     std::string username;
@@ -9,42 +12,37 @@ struct User{
     std::string password;
 
     //----------------- other variables. you can add variables in this area
-    };
+};
 
 struct Login {
 
     Login();
 
     ~Login();
-    
-    void readFile(std::string path); // reads the .txt file, then register the user inside readFile using theregisterUser function
-    
-    
-    bool checkUsername(std::string &new_username); // checks if the user is already taken, if so, it returns true and the username shouldn't be created
-    //and prints "username already taken"
 
-    bool checkEmail(std::string &new_email); // only new emails may be pushedback to "emails" vector
-    
+    void readFile(std::string path); // reads the .txt file, then register the user inside readFile using theregisterUser function
+
+    bool checkUsername(std::string& new_username); // checks if the user is already taken, if so, it returns true and the username shouldn't be created
+    // and prints "username already taken"
+
+    bool checkEmail(std::string& new_email); // only new emails may be pushedback to "emails" vector
+
     void changeUsername(std::string username, std::string newUsername);
 
     void changePassword(std::string username, std::string newPassword);
 
     void changeMail(std::string username, std::string newMail);
-    
+
     void registerUser(std::string username, std::string password, std::string email); // function to creat a user
 
     void loginUser(std::string username, std::string password);
 
     void removeUser(std::string username);
 
-    User* getUser (std::string username); // returns the usernames using the same email, returns false if that email doesnt exist
-
-
+    User* getUser(std::string username); // returns the usernames using the same email, returns false if that email doesnt exist
 
     std::vector<User*> users;
     std::vector<User*> LoginedUsers;
-
-
 };
 
 /*
--- src/hw2.cpp
+++ src/hw2.cpp
@@ -1,1 +1,15 @@
 #include "../include/hw2.h"
+
+//********** User **********//
+
+User::User(std::string name, std::string pass, std::string mail)
+    : username { name } // Initializing
+    , email { mail }
+    , password { pass }
+
+{
+}
+
+User::~User()
+{
+}
